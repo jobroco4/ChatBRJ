0f97af339c34defc5b526a7abc36c3fcdeefd2a0
Brian J <jobroco4@gmail.com>
2025-10-02 16:59:44 -0600

    new

--- main.py
+++ main.py
@@ -1,19 +1,35 @@
 import os
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+import sys
+import argparse
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
-
-from google import genai
-
 client = genai.Client(api_key=api_key)
 
+user_prompt = sys.argv[1:]    
+messages = [
+    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
 
-response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-p_tokens = response.usage_metadata.prompt_token_count
-r_tokens = response.usage_metadata.candidates_token_count
-print(response.text)
-print(f"Prompt tokens: {p_tokens}")
-print(f"Response tokens: {r_tokens}")
+if len(sys.argv) > 1:
+    response = client.models.generate_content(
+    model="gemini-2.0-flash-001",
+    contents=messages,
+    )
+    p_tokens = response.usage_metadata.prompt_token_count
+    r_tokens = response.usage_metadata.candidates_token_count
+    print(response.text)
+else:
+    raise Exception("No input provided. Please provide a prompt as a command line argument.")
+
+parser = argparse.ArgumentParser(description="Process some arguments.")
+parser.add_argument('--verbose', type=str, help='Enable verbose output')
+args = parser.parse_args()
+if args.verbose and args.verbose.lower() == 'true':
+    print(f"User prompt: {user_prompt}")   
+    print(f"Prompt tokens: {p_tokens}")
+    print(f"Response tokens: {r_tokens}")
+

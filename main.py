9e797ee86371c757b0b188b81cdb2f005fa4fb3c
Brian J <jobroco4@gmail.com>
2025-10-02 18:22:34 -0600

    new input for saving conversation history

--- main.py
+++ main.py
@@ -9,27 +9,31 @@ load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 client = genai.Client(api_key=api_key)
 
-user_prompt = sys.argv[1:]    
+user_prompt = sys.argv[1]    
+
+parser = argparse.ArgumentParser(description="Generate content using Gemini API")
+parser.add_argument("prompt", type=str, help="User prompt for Gemini")
+parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+args = parser.parse_args()
+
 messages = [
-    types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    types.Content(role="user", parts=[types.Part(text=args.prompt)]),
     ]
 
+response = client.models.generate_content(
+            model="gemini-2.0-flash-001",
+            contents=messages,
+            )
+
+print(response.text)
+
 if len(sys.argv) > 1:
-    response = client.models.generate_content(
-    model="gemini-2.0-flash-001",
-    contents=messages,
-    )
-    p_tokens = response.usage_metadata.prompt_token_count
-    r_tokens = response.usage_metadata.candidates_token_count
-    print(response.text)
+ 
+    if args.verbose:
+        print(f"User prompt: {args.prompt}")   
+        print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {response.usage_metadata.candidates_token_count}")    
 else:
     raise Exception("No input provided. Please provide a prompt as a command line argument.")
 
-parser = argparse.ArgumentParser(description="Process some arguments.")
-parser.add_argument('--verbose', type=str, help='Enable verbose output')
-args = parser.parse_args()
-if args.verbose and args.verbose.lower() == 'true':
-    print(f"User prompt: {user_prompt}")   
-    print(f"Prompt tokens: {p_tokens}")
-    print(f"Response tokens: {r_tokens}")
 
